87a205b89c6a0697cb5ab6e4a5d7670adbfc44c2
Nina <dawachung@usp.br>
2024-11-13 22:11:49 -0300

    apenas com views funcionais, sem utilizar Django forms e sem validação de dados

--- postagens/urls.py
+++ postagens/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('search/', views.search_postagens, name='search'),
     path('create/', views.create_postagem, name='create'),
     path('<int:postagem_id>/', views.detail_postagem, name='detail'),
+    path('update/<int:postagem_id>/', views.update_postagem, name='update'),
+    path('delete/<int:postagem_id>/', views.delete_postagem, name='delete'),
 ]
