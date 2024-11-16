fc26746a0f8a0d30dea503da8487feb164e2d6e4
Nina <dawachung@usp.br>
2024-11-16 19:44:45 -0300

    começando a implementar no Render

--- blog/settings.py
+++ blog/settings.py
@@ -131,7 +131,9 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static'), 
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
